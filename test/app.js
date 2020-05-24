2aba4a7a303fcba7c7221b29ded6fdf80a0eebef
Kevin Kayisire <kaykevin1@gmail.com>
2020-05-24 14:40:44 +0200

    Added a test file

---
+++ test/app.js
@@ -0,0 +1,20 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let app = require("../app");
+let should = chai.should();
+
+// Judgment starts here
+chai.use(chaiHttp);
+
+// Describing our test
+describe("/GET home", () => {
+  it("It should welcome user", (done) => {
+    chai
+      .request(app)
+      .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
